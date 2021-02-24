bc8513607dcefed678b3d181ea84bdf2ffa8f0d7
현석 송 <jhn06046@hotmail.com>
2021-02-24 19:56:47 +0900

    프로젝트

---
+++ 9.Express로SNS서비스만들기/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');   // 환경 변수 관리하는 패키지
+
+// dotenv는 require하고 나서 제일 위에 적용해 주어야 함
+dotenv.config();
+const pageRouter = require('./routes/page');
+
+const app = express();
+// 배포 시 port를 다르게 설정하기 위함
+app.set('port', process.env.PORT || 8001);
+// nunjucks 설정
+app.set('view engine','html');
+nunjucks.configure('views',{
+    express: app,
+    watch: true,
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+}));
+
+// localhost:8001/은 pageROuter로 넘겨줌
+app.use('/',pageRouter);
+
+// 404 처리 미들웨어
+app.use((req,res,next)=>{
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+    error.status = 404;
+    next(error);
+});
+
+// error 처리 미들웨어
+app.use((err, req, res, next)=>{
+    res.locals.message = err.message;
+    // 개발 모드일 때에만 stack trace를 보여주게 설정함
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+// port에 연결
+app.listen(app.get('port'),() => {
+    console.log(app.get('port'),'번 포트에서 대기중');
+});
---
+++ 9.Express로SNS서비스만들기/routes/page.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const router = express.Router();
+
+// 팔로워, 팔로잉 구현 시 사용
+router.use((req, res, next)=>{
+    res.locals.user = null;
+    res.locals.followerCount = 0;
+    res.locals.floolwingCount = 0;
+    res.locals.followerIdList = [];
+    next();
+});
+
+// app.js의 app.use가 '.'이므로 router에서도 앞에 붙는것이 없다.
+// 만약 app.js의 app.use가 /user로 연결된다면 router.get에서도 /user/profile 등으로 붙음
+// profile로 가면 프로필로 랜더링
+router.get('/profile',(req,res)=>{
+    res.render('profile',{title:'내 정보 = NodeBIrd'});
+})
+
+// join으로 가면 join으로 랜더링
+router.get('/join',(req,res)=>{
+    res.render('join',{title: '회원가입 = NodeBird'});
+})
+
+// 메인 화면을 보여줌
+router.get('/',(req,res,next)=>{
+    // twits는 메인 게시물을 넣을 공간
+    const twits = [];
+    res.render('main',{
+        title: 'NodeBird',
+        twits,
+    });
+});
+
+module.exports = router;
