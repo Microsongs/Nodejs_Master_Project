a9b98db2191936a3fa690bfcec2d7c4cc105c1c2
현석 송 <jhn06046@hotmail.com>
2021-03-22 20:42:40 +0900

    jwt

--- 10.API서버만들기/API서버/routes/middlewares.js
+++ 10.API서버만들기/API서버/routes/middlewares.js
@@ -1,3 +1,6 @@
+// 여러 라우터들에서 재사용할 미들웨어들
+const jwt = require('jsonwebtoken');
+
 // 로그인 했는지 판단
 exports.isLoggedIn = (req, res, next) => {
     // true면 로그인 되어있음
@@ -19,3 +22,23 @@ exports.isNotLoggedIn = (req, res, next)=>{
         res.redirect(`/?error=${message}`);
     }
 };
+
+exports.verifyToken = (req, res, next) => {
+    try{
+        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        return next();
+    }
+    catch(error){
+        // 유효 시간이 초과한 경우
+        if(error.name === 'TokenExpiredError'){
+            return res.ststus(419).json({
+                code: 419,
+                message: '토큰이 만료되었습니다.',
+            });
+        }
+        return res.status(401).json({
+            code: 401,
+            message: '유효하지 않은 토큰입니다.',
+        });
+    }
+}
