6c91b42df44f3375add86d8fa5c9bdacbbb2ec4b
현석 송 <jhn06046@hotmail.com>
2021-03-30 15:38:47 +0900

    cors끝

--- 10.API서버만들기/API서버/routes/v2.js
+++ 10.API서버만들기/API서버/routes/v2.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+// CORS 방지용
+const cors = require('cors');
+const url = require('url');
 
 const { verifyToken, apiLimiter } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
+const { urlencoded } = require('express');
 
 const router = express.Router();
-// CORS 방지용
-const cors = require('cors');
+
+/*
+// 아래 방법은 모든 것을 허용함 -> 처음부터 없는 것이랑 다름이 없음
 router.use(cors({
+    origin: true,
     credentials: true,
 }));
+*/
+// 미들웨어 확장 패턴으로 cors 미들웨어를 확장
+router.use(async (req, res, next) => {
+    const domain = await Domain.findOne({
+        // 요청 보낼떄 설정한 origin을 꺼내 host를 설정 ?는 앞이 undefined면 그대로, 객체면 뒤에서 host 꺼내옴
+        where: {host: url.parse(req.get('origin'))?.host }
+    });
+    // domain에서 요청이 온 경우 cors모듈을 붙여줌
+    if(domain){
+        cors({
+            origin: true,
+            credentials: true,
+        })(req, res, next);
+    }
+    else{
+        next();
+    }
+})
+
 
 // 토큰 발급 라우트
 router.post('/token', apiLimiter, async(req, res) => {
